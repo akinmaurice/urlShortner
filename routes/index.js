52afba8bc66f9f7f99e85385610714c2bfdf6eac
akinmaurice <akintayoy@gmail.com>
2017-12-11 22:31:17 +0100

    Added API Endpoints to handle the React App

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 const appController = require('../controllers/appController');
+const apiController = require('../controllers/api');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 /* GET home page. */
 router.get('/', appController.getIndex);
 
 /* Router to Shorten URL */
-router.post('/',
-    appController.validateInput,
-    appController.isUrlValid,
-    catchErrors(appController.newUrl),
-    appController.shortenUrl
+router.post(
+  '/',
+  appController.validateInput,
+  appController.isUrlValid,
+  catchErrors(appController.newUrl),
+  appController.shortenUrl,
 );
 
 /* GET Real Link. */
 router.get('/:shortUrl', catchErrors(appController.getUrl));
+
+
+router.post(
+  '/api',
+  apiController.isUrlValid,
+  catchErrors(apiController.newUrl),
+  apiController.shortenUrl,
+);
 
 module.exports = router;
